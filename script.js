55c22c8e6102c4a68b4b647e4ce4317109e417b3
BlendiHoxha24 <149906457+BlendiHoxha24@users.noreply.github.com>
2023-11-07 00:02:21 +0100

    news

---
+++ script.js
@@ -0,0 +1,10 @@
+const hiddenLinks = document.getElementById('HiddenLinks');
+const menuButton = document.getElementById('MenuButton');
+const closeMenuButton =document.getElementById('CloseMenuButton');
+
+menuButton.addEventListener('click',() => {
+    hiddenLinks.classList.remove('hide');
+})
+closeMenuButton.addEventListener('click',() =>{
+    hiddenLinks.classList.add('hide');
+})
